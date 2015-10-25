4c5f14de54c008d8f4723fa125ee3e532df51e5f
RussellLuo <luopeng.he@gmail.com>
2015-10-25 13:27:09 +0800

    Implement the `get_embedded_rules` method of FalconAdapter

--- restart_falcon/adapter.py
+++ restart_falcon/adapter.py
@@ -18,22 +18,8 @@ class FalconAdapter(Adapter):
     def __init__(self, *args, **kwargs):
         super(FalconAdapter, self).__init__(*args, **kwargs)
         self.falcon_api = FalconAPI()
-        self.add_routes()
-
-    def add_routes(self):
-        """Add Falcon-specific URI routes."""
-        for endpoint, rule in iteritems(self.adapted_rules):
-            resource = FalconResource()
-            for method in rule.methods:
-                on_method = types.MethodType(rule.handler, resource)
-                setattr(resource, 'on_' + method.lower(), on_method)
-            # Convert URI style from RESTArt's `<arg>` to Falcon's `{arg}`
-            uri = rule.uri.replace('<', '{').replace('>', '}')
-            assert ':' not in uri, (
-                'Werkzeug-style converters (with the separator ":") is '
-                'not supported in `RESTArt-Falcon`. Captured parameters '
-                'in URI can only be specified in the form of "<arg>"'
-            )
+        # Add Falcon-specific URI routes
+        for uri, resource in self.get_embedded_rules():
             self.falcon_api.add_route(uri, resource)
 
     def adapt_handler(self, handler, resource, request,
@@ -65,3 +51,42 @@ class FalconAdapter(Adapter):
         meanings of the parameters.
         """
         return self.falcon_api(environ, start_response)
+
+    def get_embedded_rules(self):
+        """Get the Falcon-specific rules used to be embedded into
+        an existing or legacy application.
+
+        Usage:
+
+            # The existing Falcon API
+            import falcon
+            app = falcon.API()
+            ...
+
+            # The RESTArt API
+            from restart.api import RESTArt
+            api = RESTArt()
+            ...
+
+            # Embed RESTArt into Falcon
+            from restart.serving import Service
+            from restart.ext.falcon.adapter import FalconAdapter
+            service = Service(api, FalconAdapter)
+            for uri, resource in service.embedded_rules:
+                app.add_route(ui, resource)
+        """
+        rules = []
+        for endpoint, rule in iteritems(self.adapted_rules):
+            resource = FalconResource()
+            for method in rule.methods:
+                on_method = types.MethodType(rule.handler, resource)
+                setattr(resource, 'on_' + method.lower(), on_method)
+            # Convert URI style from RESTArt's `<arg>` to Falcon's `{arg}`
+            uri = rule.uri.replace('<', '{').replace('>', '}')
+            assert ':' not in uri, (
+                'Werkzeug-style converters (with the separator ":") is '
+                'not supported in `RESTArt-Falcon`. Captured parameters '
+                'in URI can only be specified in the form of "<arg>"'
+            )
+            rules.append((uri, resource))
+        return tuple(rules)
